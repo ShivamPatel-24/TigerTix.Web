0bf857787dd894dff24eab9931d72aad47834a14
Shivam <shivampatel2473@gmail.com>
2022-05-02 00:44:59 -0400

    try to debug

--- app.js
+++ app.js
@@ -211,3 +211,7 @@ if (port == null || port == ""){
 app.listen(port, function(){
     console.log('Server started on port successfully')
 });
+
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
